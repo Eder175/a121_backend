e83d4609bb1d394420a686d72e69ee3a12ab2a5d
Eder <souzaeder175@gmail.com>
2025-03-03 17:50:00 +0000

    Correção do arquivo styles.css e atualização do layout

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,8 @@ from . import views  # Importa as views do app core
 
 urlpatterns = [
     path('', views.home, name='home'),  # Rota para a view "home"
+    path('escritorio/', views.escritorio, name='escritorio'),  # Rota para o escritório
+    path('cadastro/', views.cadastro, name='cadastro'),  # Rota para o cadastro
+    path('login/', views.login, name='login'),  # Rota para o login
 ]
-
 
--- core/views.py
+++ core/views.py
@@ -1,6 +1,13 @@
-# core/views.py
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/index.html')
 
+def escritorio(request):
+    return render(request, 'core/escritorio.html')
+
+def cadastro(request):
+    return render(request, 'core/signup.html')
+
+def login(request):
+    return render(request, 'core/login.html')
