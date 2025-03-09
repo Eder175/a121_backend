c98a0b5215457d5f79f4b7223e62032032e19269
Eder <souzaeder175@gmail.com>
2025-03-09 13:52:40 +0000

    Restaurar layout original e corrigir iPhone 3D na seção hero

--- a121_backend/settings.py
+++ a121_backend/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
 
 # Configuração de idioma e fuso horário
 LANGUAGE_CODE = 'pt-br'
-TIME_ZONE = 'America/Sao_Pao Paulo'
+TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
 
