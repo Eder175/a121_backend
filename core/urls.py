851092c63c80d11df464b47f7a0f3fdcea8da07b
Eder <souzaeder175@gmail.com>
2025-03-12 02:57:40 +0000

    Atualização do site: melhorias no design e funcionalidades

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,13 @@
-# Substitua o conteúdo de core/urls.py
 from django.urls import path
-from . import views  # Importa as views do app core
+from . import views
+
+app_name = 'core'
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Rota para a view "home"
-    path('escritorio/', views.escritorio, name='escritorio'),  # Rota para o escritório
-    path('cadastro/', views.cadastro, name='cadastro'),  # Rota para o cadastro
-    path('login/', views.login, name='login'),  # Rota para o login
-    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('', views.home, name='home'),
+    path('escritorio/', views.escritorio, name='escritorio'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('login/', views.login, name='login'),
+    path('produto/<int:product_id>/', views.product_detail, name='product_detail'),
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
