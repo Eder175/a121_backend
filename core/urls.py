d12e7aa2d1393658b5b29a4cf60005bc792337a3
Eder <souzaeder175@gmail.com>
2025-03-13 23:46:54 +0000

    Corrigido namespace 'core' em a121_backend/urls.py e restaurado index.html completo

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('cursos/', views.cursos, name='cursos'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.login, name='login'),
-    path('change_currency/', views.change_currency, name='change_currency'),  # Rota para mudar moeda
-    path('get_exchange_rate/', views.get_exchange_rate, name='get_exchange_rate'),  # Rota para pegar taxa
+    path('change_currency/', views.change_currency, name='change_currency'),
+    path('get_exchange_rate/', views.get_exchange_rate, name='get_exchange_rate'),
 ]
