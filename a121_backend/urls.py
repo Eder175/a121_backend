1732d2c4d9ae0fd1e302a7c40200e39c71e35cc7
Eder <souzaeder175@gmail.com>
2025-03-13 23:37:12 +0000

    Atualizados views.py, urls.py e index.html para corrigir NoReverseMatch e suportar mudança de moeda

--- a121_backend/urls.py
+++ a121_backend/urls.py
@@ -1,38 +1,7 @@
-"""
-URL configuration for a121_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
-# Define as rotas principais do projeto
 urlpatterns = [
-    path('admin/', admin.site.urls),  # URL para o painel de administração do Django
-    path('', include('core.urls')),  # Inclui as URLs do app core (definidas em core/urls.py)
-    path('login/', include('core.urls')),  # Inclui as URLs para login
-    path('cadastro/', include('core.urls')),  # Inclui as URLs para cadastro
-    path('cursos/', include('core.urls')),  # Inclui as URLs para cursos
-    path('dashboard/', include('core.urls')),  # Inclui as URLs para dashboard
-    path('escritorio/', include('core.urls')),  # Inclui as URLs para escritório
-    path('produto/<int:product_id>/', include('core.urls')),  # Inclui as URLs para detalhes de produto
-    path('ganhar-pontos/', include('core.urls')),  # Inclui as URLs para ganhar pontos
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
 ]
-
-# Serve arquivos estáticos e de mídia durante o desenvolvimento (DEBUG=True)
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
