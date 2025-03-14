cc0cd03d4c8a9565ddc85638dc9b6693107cc2c2
Eder <souzaeder175@gmail.com>
2025-03-14 10:18:44 +0000

    Corrigido erro NoReverseMatch, adicionados formulários reais para cadastro e login, e corrigido caminho das imagens

--- a121_backend/urls.py
+++ a121_backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.i18n import JavaScriptCatalog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls', namespace='core')),
+    path('', include('core.urls')),
+    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),  # Adicionando suporte para traduções JS
 ]
