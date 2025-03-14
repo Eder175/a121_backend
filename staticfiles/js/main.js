2e29c61928127ebb132e0c758fb5c5e8d6a21c59
Eder <souzaeder175@gmail.com>
2025-03-14 13:18:15 +0000

    Limpa repositório: remove arquivos antigos, atualiza estilos e templates

---
+++ staticfiles/js/main.js
@@ -0,0 +1,9 @@
+// Efeito de digitação no título
+document.addEventListener('DOMContentLoaded', () => {
+    new Typed('.typed-text', {
+        strings: ['A121', 'Inovação', 'Futuro', 'Revolução'],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true
+    });
+});
