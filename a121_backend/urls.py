37c7e8651bc8c0fc004001b593ff373fc31b0cbf
Eder <souzaeder175@gmail.com>
2025-03-14 00:19:52 +0000

    Corrigido ImportError para i18n e restaurado index.html completo com 245 linhas

--- a121_backend/urls.py
+++ a121_backend/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns  # Importação correta
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls', namespace='core')),  # Namespace 'core' será válido com app_name em core/urls.py
-]
+    path('i18n/', include('django.conf.urls.i18n')),  # Para suportar 'javascript-catalog'
+] + i18n_patterns(
+    path('', include('core.urls', namespace='core')),
+    prefix_default_language=False,
+)
