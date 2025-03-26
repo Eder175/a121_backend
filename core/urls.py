0802947358cc2fe0ed2f8228b8f1740b0717e6a9
Eder <souzaeder175@gmail.com>
2025-03-26 19:39:49 +0000

    Adiciona namespace 'translation' em core/urls.py para resolver erro de namespace

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+# core/urls.py
+from django.urls import path, include
 from core import views
 
 app_name = 'core'
 
 urlpatterns = [
+    # Páginas principais
     path('', views.index, name='index'),  # Mapeia para index.html
     path('cursos/', views.cursos, name='cursos'),  # Mapeia para cursos.html
     path('cadastro/', views.cadastro, name='cadastro'),  # Mapeia para cadastro.html
@@ -14,7 +16,14 @@ urlpatterns = [
     path('ganhar-pontos/', views.ganhar_pontos, name='ganhar_pontos'),  # Mapeia para ganhar_pontos.html
     path('signup/', views.signup, name='signup'),  # Mapeia para signup.html
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),  # Mapeia para product_detail.html
+
+    # Funcionalidades
     path('change_currency/', views.change_currency, name='change_currency'),
     path('get_exchange_rate/', views.get_exchange_rate, name='get_exchange_rate'),
     path('chat/', views.chat_interaction, name='chat_interaction'),
+
+    # Namespace para tradução
+    path('translation/', include([
+        path('translate_video/', views.translate_video, name='translate_video'),
+    ], namespace='translation')),
 ]
